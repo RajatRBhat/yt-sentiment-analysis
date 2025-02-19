e5bc0ca7332a07676a803e46a015e2301905c8ef
Rajat Bhat <rajatbhat@Rajats-MacBook-Air.local>
2025-02-19 23:04:43 +0530

    add ci workflow

--- test.py
+++
@@ -1,5 +0,0 @@
-import joblib
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-comments = ["I love this video", "i hate this video"]
-transformed_comments = vectorizer.transform(comments)
-print(transformed_comments)
