a5579e36cdaecaf6c38277bd802f2004ddb2ba0c
Rajat Bhat <rajatbhat@Rajats-MacBook-Air.local>
2025-02-20 00:27:37 +0530

    flask api test

--- flask_app/app.py
+++ flask_app/app.py
@@ -53,7 +53,7 @@ def preprocess_comment(comment):
 # Load the model and vectorizer from the model registry and local storage
 def load_model_and_vectorizer(model_name, model_version, vectorizer_path):
     # Set MLflow tracking URI to your server
-    mlflow.set_tracking_uri("http://127.0.0.1:8080/")  # Replace with your MLflow tracking URI
+    mlflow.set_tracking_uri("http://ec2-52-87-241-200.compute-1.amazonaws.com:5000/")  # Replace with your MLflow tracking URI
     client = MlflowClient()
     model_uri = f"models:/{model_name}/{model_version}"
     model = mlflow.sklearn.load_model(model_uri)
